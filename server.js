3c9514c9fb33dab3511bd4cc8a767f400b41a874
Green Kim <greenkimparsons@gmail.com>
2021-04-16 16:16:07 -0400

    fixed css not working on notes page

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const path = require("path");
+const notes = require("./db/db.json");
 
  
 const app = express();
@@ -8,6 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
 require('./routes/apiRoutes')(app);
