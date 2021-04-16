46f94c9dfefcad8bc548574da426aba1b450b22b
Green Kim <greenkimparsons@gmail.com>
2021-04-16 19:24:53 -0400

    got test json to show on notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const notes = require("../db/db.json");
+const path = require("path");
 
 module.exports = (app) => {
   // when they  click on the API Table and API Wait List links, it shows you the json
@@ -8,19 +9,19 @@ module.exports = (app) => {
     noteData.push(request.body); //pushing the new obj value into database
   });
 
-  app.get("/api/notes", (request, respond) => {
-    response.sendFile(path.join(__dirname, "/db/db.json"))
+  app.get("/api/notes", (request, response) => {
+    response.sendFile(path.join(__dirname, "../db/db.json"))
   });
   
-  app.post("/api/notes", (request, respond) => {
-    const notes = JSON.parse(fs.readFileSync("/db/db.json"));
+  app.post("/api/notes", (request, response) => {
+    const notes = JSON.parse(fs.readFileSync("../db/db.json"));
     console.log(notes);
     // user's new notes
     const newNotes = request.body;
     // adding the new notes into the json
     notes.push(newNotes);
     // create a new file and writing it into db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    fs.writeFileSync("../db/db.json", JSON.stringify(notes))
     // server responding with updates notes file
     response.json(notes);
   });
