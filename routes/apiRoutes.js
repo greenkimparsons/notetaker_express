36ebfb14294381d7948aa82a9d1bb8dbafe532db
Green Kim <greenkimparsons@gmail.com>
2021-04-16 20:24:17 -0400

    went over with tutor

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,11 @@
 const notes = require("../db/db.json");
 const path = require("path");
 const fs = require("fs");
+// const uuidv4 = require("uuid/v4");
 
 module.exports = (app) => {
   // when they  click on the API Table and API Wait List links, it shows you the json
-  app.get("api/notes", (request, response) => response.json(noteData));
+  // app.get("api/notes", (request, response) => response.json(noteData));
 
   app.get("api/notes", (request, response) => {
     noteData.push(request.body); //pushing the new obj value into database
@@ -15,32 +16,23 @@ module.exports = (app) => {
   });
   
   app.post("/api/notes", (request, response) => {
-    const notes = JSON.parse(fs.readFileSync("../db/db.json"));
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
     console.log(notes);
     // user's new notes
     const newNotes = request.body;
+    // newNotes.id = uuidv4();
     // adding the new notes into the json
     notes.push(newNotes);
     // create a new file and writing it into db.json
-    fs.writeFileSync("../db/db.json", JSON.stringify(notes))
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes))
     // server responding with updates notes file
     response.json(notes);
   });
+  
+  localhost/apai/notees/5
   //delete route - /notes/:delete?
-
-  const deleteNote = () => {
-    console.log('Deleting note by id...\n');
-    connection.query(
-      'DELETE FROM products WHERE ?',
-      {
-        id: 'strawberry',
-      },
-      (err, res) => {
-        if (err) throw err;
-        console.log(`${res.affectedRows} products deleted!\n`);
-        // Call readProducts AFTER the DELETE completes
-        readProducts();
-      }
-    );
-  };
+  app.delete("/api/notes/:id", (request, response) => {
+    request.params.id
+    response.json(deleteNote);
+  });
 };
